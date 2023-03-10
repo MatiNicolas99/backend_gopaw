07b944a8ea4e3816adcb4333c3632280b91c671a
apaezp <andrea.paez86@gmail.com>
2023-03-10 15:38:52 -0300

    Updating unit tests

--- controllers/appcontroller.test.js
+++ controllers/appcontroller.test.js
@@ -1,230 +1,211 @@
-const request = require('supertest');
-const app = require('../app');
-const jwt = require('jsonwebtoken');
+const request = require("supertest");
+const app = require("../app");
+const jwt = require("jsonwebtoken");
 const pool = require("../config/appconfig");
 const {
-        appGetVeterinarys,
-        appGetReviews,
-        appGetOwnerById,
-        appGetVeterinaryById,
-        appGetPetAppointments,
-        appGetVeterinaryAppointments,
-        appLogin,       
-        nuevoUsuario,
-        nuevoVet,
-        nuevaReseña,
-        nuevaMascota,
-        nuevoAppointment,        
-        appDelAppointmentById,
-        appDelReviewById,        
-        appPutReview,
-        appPutOwner,
-        appPutVeterinary,
-        appPutOwnerPassword,
-        appPutVeterinaryPassword} = require('./appcontroller')
+  appGetVeterinarys,
+  appGetReviews,
+  appDelReviewById,
+  
+} = require("./appcontroller");
 
-const { verificarCredenciales } = require('../models/appmodel');
+const waitForPort = require("wait-for-port");
 
-const waitForPort = require('wait-for-port');
-
-describe('my server', () => {
+describe("my server", () => {
   beforeAll(async () => {
     await new Promise((resolve) => {
-       waitForPort('localhost', 8080, resolve);
-        });
+      waitForPort("localhost", 8080, resolve);
     });
-        
+  });
+
   afterEach(() => {
-    jest.resetAllMocks();  
-  });  
-             
+    jest.resetAllMocks();
+  });
+
+  jest.mock("./appcontroller.js");
+
+  describe("appGetVeterinarys", () => {
+    test("should return an array of veterinarys", async () => {
+      const getVeterinarys = jest.fn();
+      const mockVeterinarys = [
+        {
+          id: 4,
+          veterinary_name: "pedro",
+          phone: "123412345",
+          email: "pedro@gmail.com",
+          image: null,
+        },
+        {
+          id: 6,
+          veterinary_name: "pedro",
+          phone: "123412345",
+          email: "pedro@gmail.com",
+          image: null,
+        },
+        {
+          id: 2,
+          veterinary_name: "sanchoPanza",
+          phone: "99988877744",
+          email: "sancho@panza.com",
+          image: null,
+        },
+        {
+          email: "test@gmail.com",
+          id: 8,
+          image: null,
+          phone: "123123123",
+          veterinary_name: "test",
+        },
+      ];
+      getVeterinarys.mockResolvedValue(mockVeterinarys);
+      const req = {};
+      const res = { json: jest.fn() };
+      await appGetVeterinarys(req, res);
+      expect(res.json).toHaveBeenCalledWith(mockVeterinarys);
+    });
+
+    test("should return an error response if getVeterinarys throws an error", async () => {
+      const getVeterinarys = jest.fn();
+      const mockError = { message: "Something went wrong" };
+      getVeterinarys.mockRejectedValue(mockError);
+      const req = {};
+      const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
+      await appGetVeterinarys(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith(mockError);
+    });
+  });
+
+  describe("appGetReviews", () => {
+    test("should return an array of reviews", async () => {
+      const mockReviews = [
+        {
+          id: 3,
+          date: "12/30/2023",
+          title: "review test",
+          content: "asdasdasdasdassdasdasdasdasdasdasdas",
+          owner_id: 5,
+          veterinary_id: 6,
+        },
+        {
+          id: 1,
+          date: "12/31/2023",
+          title: "edicion de review",
+          content: "se edita el review",
+          owner_id: 1,
+          veterinary_id: 2,
+        },
+      ];
+
+      global.fetch = jest.fn(() =>
+        Promise.resolve({
+          json: () => Promise.resolve(mockReviews),
+        })
+      );
+
+      const req = {};
+      const res = {
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+
+      await appGetReviews(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(mockReviews);
+    });
+  });
+
+  describe("appDelReviewById", () => {
+    test("should delete review successfully", async () => {
+      const mockReq = {
+        body: { id: 1 },
+        header: () => "Bearer mockToken",
+      };
+      const mockRes = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      };
   
-jest.mock('./appcontroller.js');
+      jest.spyOn(jwt, "verify").mockImplementation(() => {});
+      jest.spyOn(pool, "query").mockImplementation(() => {
+        return { rowCount: 1 };
+      });
+  
+      await appDelReviewById(mockReq, mockRes);
+  
+      expect(jwt.verify).toHaveBeenCalled();
+      expect(pool.query).toHaveBeenCalledWith(
+        "DELETE FROM review WHERE id = $1",
+        [1]
+      );
+      expect(mockRes.send).toHaveBeenCalledWith("Review eliminado con éxito");
+    });
+  
+    test("should throw error if review not found", async () => {
+      const mockReq = {
+        body: { id: 1 },
+        header: () => "Bearer mockToken",
+      };
+      const mockRes = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      };
+  
+      jest.spyOn(jwt, "verify").mockImplementation(() => {});
+      jest.spyOn(pool, "query").mockImplementation(() => {
+        return { rowCount: 0 };
+      });
+  
+      await appDelReviewById(mockReq, mockRes);
+  
+      expect(mockRes.status).toHaveBeenCalledWith(404);
+      expect(mockRes.send).toHaveBeenCalledWith({
+        code: 404,
+        message: "No se encontró ningún review con este ID",
+      });
+    });
+  
+    test("should throw error if authorization fails", async () => {
+      const mockReq = {
+        body: { id: 1 },
+        header: () => "Bearer invalidToken",
+      };
+      const mockRes = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      };
+  
+      jest.spyOn(jwt, "verify").mockImplementation(() => {
+        throw new Error("invalid token");
+      });
+  
+      await appDelReviewById(mockReq, mockRes);
+  
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith(expect.any(Error));
+    });
+  
+    test("should throw error if query fails", async () => {
+      const mockReq = {
+        body: { id: 1 },
+        header: () => "Bearer mockToken",
+      };
+      const mockRes = {
+        send: jest.fn(),
+        status: jest.fn().mockReturnThis(),
+      };
+  
+      jest.spyOn(jwt, "verify").mockImplementation(() => {});
+      jest.spyOn(pool, "query").mockImplementation(() => {
+        throw new Error("query error");
+      });
+  
+      await appDelReviewById(mockReq, mockRes);
+  
+      expect(mockRes.status).toHaveBeenCalledWith(500);
+      expect(mockRes.send).toHaveBeenCalledWith(expect.any(Error));
+    });
+  });
 
-    describe('appGetVeterinarys', () => {
-          test('should return an array of veterinarys', async () => {           
-            const getVeterinarys = jest.fn();
-            const mockVeterinarys = [
-              {
-                "id": 4,
-                "veterinary_name": "pedro",
-                "phone": "123412345",
-                "email": "pedro@gmail.com",
-                "image": null
-              },             
-              {
-                "id": 6,
-                "veterinary_name": "pedro",
-                "phone": "123412345",
-                "email": "pedro@gmail.com",
-                "image": null
-              },
-              {
-                "id": 2,
-                "veterinary_name": "sanchoPanza",
-                "phone": "99988877744",
-                "email": "sancho@panza.com",
-                "image": null
-              },              
-            ];
-           getVeterinarys.mockResolvedValue(mockVeterinarys);
-            const req = {};
-            const res = { json: jest.fn() };
-            await appGetVeterinarys(req, res);
-            expect(res.json).toHaveBeenCalledWith(mockVeterinarys);
-          });
-        
-          test('should return an error response if getVeterinarys throws an error', async () => {
-            const getVeterinarys = jest.fn();            
-            const mockError = ({message:'Something went wrong'});
-           getVeterinarys.mockRejectedValue(mockError);
-            const req = {};
-            const res = { status: jest.fn().mockReturnThis(), send: jest.fn() };
-            await appGetVeterinarys(req, res);
-            expect(res.status).toHaveBeenCalledWith(500);
-            expect(res.send).toHaveBeenCalledWith(mockError);
-          });
-        });
-
-    describe('appGetReviews', () => {
-          test('should return an array of reviews', async () => {
-            const mockReviews = [
-              {
-                id: 3,
-                date: '12/30/2023',
-                title: 'review test',
-                content: 'asdasdasdasdassdasdasdasdasdasdasdas',
-                owner_id: 5,
-                veterinary_id: 6,
-              },
-              {
-                id: 1,
-                date: '12/31/2023',
-                title: 'edicion de review',
-                content: 'se edita el review',
-                owner_id: 1,
-                veterinary_id: 2,
-              },
-            ];
-        
-            global.fetch = jest.fn(() =>
-              Promise.resolve({
-                json: () => Promise.resolve(mockReviews),
-              })
-            );
-        
-            const req = {};
-            const res = {
-              json: jest.fn(),
-              status: jest.fn().mockReturnThis(),
-              send: jest.fn(),
-            };
-        
-            await appGetReviews(req, res);
-        
-            expect(res.json).toHaveBeenCalledWith(mockReviews);
-          });
-        });
-
-// jest.mock('jsonwebtoken');
-
-// jest.mock('./appcontroller', () => ({
-//   appLogin: jest.fn(),
-// }));
-
-//     describe('appLogin', () => {
-//       const req = {
-//         "body": {
-//           "email": "julio@gmail.com", 
-//           "password": "clave123",}
-//       };
-
-//       test('Should return a token', async () => {
-//         const appLogin = jest.fn()
-        
-//         const tokenMock = 'tokenMock';
-//         const mockSign = jest.spyOn(jwt, 'sign').mockReturnValue(tokenMock);
-//         const mockSend = jest.fn();
-
-//         console.log(appLogin)
-
-//        appLogin.mockResolvedValue();
-
-//         const res = {
-//           send: mockSend,
-//         };
-
-//         await appLogin(req,res);
-
-//         expect(appLogin).toHaveBeenCalledWith(
-//           req.body.email,
-//           req.body.password,
-//           expect.any(Object)
-//         );
-        
-//         expect(mockSign).toHaveBeenCalledWith({email: req.body.email}, "az_AZ", {expiresIn: 1800 });
-//         expect(mockSend).toHaveBeenCalledWith(tokenMock);
-//       })
-//     })
-
-// jest.mock('./appcontroller.js', () => ({
-//             delAppointmentById: jest.fn(),
-//             delReviewById: jest.fn(),
-//             editReview: jest.fn(),
-//           }));
-          
-//           describe('appDelAppointmentById', () => {
-//             test('should delete an appointment and return success message', async () => {
-//               const req = {
-//                 params: { id: '123' },
-//                 header: () => 'Bearer fake-token',
-//               };
-//               const res = {
-//                 send: jest.fn(),
-//                 status: jest.fn(),
-//               };
-//               const mockDelAppointmentById = require('./appcontroller').delAppointmentById;
-//               mockDelAppointmentById.mockResolvedValueOnce();
-            
-//               await appDelAppointmentById(req, res);
-            
-//               expect(mockDelAppointmentById).toHaveBeenCalledWith('123');
-//               expect(res.send).toHaveBeenCalledWith('Appointment eliminado con éxito');
-//             });
-          
-  //         describe('appDelReviewById', () => {
-  //           test('should delete a review and return success message', async () => {
-  //             const req = {
-  //               body: { id: '456' },
-  //               header: () => 'Bearer fake-token',
-  //             };
-  //             const res = {
-  //               send: jest.fn(),
-  //               status: jest.fn(),
-  //             };
-  //             const mockDelReviewById = require('../config/appconfig.js').delReviewById;
-  //             mockDelReviewById.mockResolvedValueOnce();
-          
-  //             await appDelReviewById(req, res);              
-          
-  //             expect(mockDelReviewById).toHaveBeenCalledWith('456');
-  //             expect(res.send).toHaveBeenCalledWith('Review eliminado con éxito');
-  //           });
-          
-  //           test('should handle errors', async () => {
-  //             const req = { body: {} };
-  //             const res = {
-  //               send: jest.fn(),
-  //               status: jest.fn(),
-  //             };
-  //             const mockDelReviewById = require('../config/appconfig.js').delReviewById;
-  //             mockDelReviewById.mockRejectedValueOnce({ code: 500 });
-          
-  //             await appDelReviewById(req, res);
-          
-  //             expect(mockDelReviewById).toHaveBeenCalledWith(undefined);
-  //             expect(res.status).toHaveBeenCalledWith(500);
-  //             expect(res.send).toHaveBeenCalledWith({ code: 500 });
-  //           });
-          // });
-   })       
+});

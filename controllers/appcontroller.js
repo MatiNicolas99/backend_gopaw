220f887941f5de84a8e2664fcec4da1ed6c3df24
MatiBriceño <bvmatiasnicolas@gmail.com>
2023-03-15 22:37:33 -0400

    Modificación de contraseña

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -263,9 +263,9 @@ const appPutOwnerPassword = async (req, res) => {
 const appPutVeterinaryPassword = async (req, res) => {
   try {
     const { id, password} = req.body;
-    const Authorization = req.header("Authorization");
-    const token = Authorization.split("Bearer ")[1];
-    jwt.verify(token, "az_AZ");
+    // const Authorization = req.header("Authorization");
+    // const token = Authorization.split("Bearer ")[1];
+    // jwt.verify(token, "az_AZ");
     await editVeterinaryPassword(id, password);
     res.send("Contraseña editada con éxito");
   } catch (error) {

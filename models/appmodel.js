a9c5ea7d5c126932c2d14819c9e3c145da93e66e
AndresAMF <andres.1991.munoz@gmail.com>
2023-03-17 01:41:13 -0300

    se añade campo hora a reserva de citas

--- models/appmodel.js
+++ models/appmodel.js
@@ -90,9 +90,9 @@ const registrarVet = async (vet) => {
 };
 
 const registrarAppointment = async (appointment) => {
-  let { date, pet_id, veterinary_id } = appointment;
-  const values = [date, pet_id, veterinary_id];
-  const consulta = "INSERT INTO appointment values (DEFAULT, $1, $2, $3)";
+  let { date, pet_id, veterinary_id, hour } = appointment;
+  const values = [date, pet_id, veterinary_id, hour];
+  const consulta = "INSERT INTO appointment values (DEFAULT, $1, $2, $3, $4)";
   await pool.query(consulta, values);
 };
 
